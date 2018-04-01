ed35805722b53c9180ee8d5bef93ae7d1742eb8d
dkilfoyle <dkilfoyle@xtra.co.nz>
2018-04-01 15:27:41 +1200

    spelling

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -6,7 +6,7 @@
         //-   q-icon(name="menu")
 
         q-toolbar-title ADHB ISU Bed Modelling
-          div(slot="subtitle") ADHB Integrated Stroke Unit Bed Modeling and Simulation E-Tool
+          div(slot="subtitle") ADHB Integrated Stroke Unit Bed Modelling and Simulation E-Tool
 
     //- q-layout-drawer(v-model="leftDrawerOpen" content-class="bg-grey-2")
     //-   q-list(no-border link inset-delimiter)
